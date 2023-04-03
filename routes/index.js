fcd71c308566416fd09521f5a0c4c5723d90eeb5
xiaowu <943915349@qq.com>
2023-04-03 16:32:44 +0800

    测试

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 import models from './chatgpt/models'
 import chat from './chatgpt/chat'
+import Router from "koa-router";
+import {sysCfg} from "../config";
+
+const router = Router({prefix: sysCfg.apiPrefix});
+
+const index = router.get('/', async (ctx) => {
+  ctx.log.info('success deploy...');
+  ctx.send('Success');
+});
 export {
+  index,
   models,
   chat
 };
